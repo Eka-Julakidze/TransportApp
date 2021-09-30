9558c98d30908ef3d62f7a430c74d9243149b685
Eka-Julakidze <ejulakidze@credo.ge>
2021-09-30 17:00:49 +0400

    Add logic for LINQ

--- TransportDriver/Program.cs
+++ TransportDriver/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using TransportNameSpace;
 using static TransportNameSpace.Transport;
 using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace TransportDriver
 {
-
     class Program
     {
         static void Main(string[] args)
@@ -23,6 +23,10 @@ namespace TransportDriver
             {
                 using (StreamReader reader = File.OpenText(inputPath))
                 {
+                    // Read transport data from input file
+                    Console.WriteLine("Reading data from input_data.csv.txt ...");
+                    Thread.Sleep(3000);
+
                     string s;
                     while ((s = reader.ReadLine()) != null)
                     {
@@ -30,29 +34,52 @@ namespace TransportDriver
                         speed = Convert.ToDouble(s.Substring(s.LastIndexOf(',') + 1));
                         if (s[0] == 'T')
                         {
+
                             vehicles.Add(new Train(s[0], name, speed));
                         }
                         else
                         {
-                            vehicles.Add(new Buss(s[0], name, speed));
+                            vehicles.Add(new Buss(s[0], name, speed));                           
                         }
+                        
+                    }
+                    Console.WriteLine("### reading: DONE");
+                    Console.WriteLine("\n### ALL THE VEHICLES ###");
+                    foreach (var vehicle in vehicles)
+                    {
+                        Console.WriteLine(vehicle.ToString());
                     }
                 }
 
+                Console.WriteLine("\n### FILTERING DATA FOR HIGH SPEED VEHICLES ...");
+                Thread.Sleep(2000);
+                // look for high speed trains and busses
+                vehicles = vehicles.Where(x =>
+                (x.TransportType == 'T' && x.Speed > 250) || // train
+                (x.TransportType == 'B' && x.Speed > 80)).ToList(); // buss
+                Console.WriteLine("### filtering: DONE");
+
+                // write data about high speed vehicles into a new file
+                Console.WriteLine("\n### Writing filtered data to output_data.txt ...");
+                Thread.Sleep(3000);
                 using (StreamWriter writer = File.CreateText(outputPath))
                 {
-                    writer.WriteLine("### OUTPUT DATA FOR ALL TRANSPORT ###");
+                    //writer.WriteLine("### OUTPUT DATA For HIGH SPEED###");
                     foreach (var v in vehicles)
                     {
                         writer.Write(v.GetType().Name + " ");
                         writer.WriteLine(v.ToString());
                     }                  
                 }
+                Console.WriteLine("### writing: DONE");
 
-                using(StreamReader reader = File.OpenText(outputPath))
+                // print out the data from newly created file to the console
+                using (StreamReader reader = File.OpenText(outputPath))
                 {
-                    Console.WriteLine("\n### READING OUTPUT DATA FOR ALL TRANSPORT ###");
+                    Console.WriteLine("\n### READING FROM output_data.txt; THESE ARE HIGH SPEED VEHICLES ONLY ###");
+                    Thread.Sleep(2500);
                     Console.WriteLine(reader.ReadToEnd().ToString());
+                    Console.WriteLine("### reading: DONE");
                 }               
             }
             catch (IOException e)
@@ -60,10 +87,15 @@ namespace TransportDriver
                 Console.WriteLine("Error reading the file");
                 Console.WriteLine(e.Message);
             }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
-           
+            
 
-           
+
+
 
         }
     }
